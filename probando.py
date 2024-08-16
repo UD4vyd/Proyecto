9435c1dafbca00d79e8aa246e72d9ba753aa9191
UD4vyd <davidmartinezrincon9@gmail.com>
2024-08-16 17:25:25 +0000

    clase 14 8

--- probando.py
+++ probando.py
@@ -3,7 +3,7 @@ print("¡Hola, mundo como estan todos!")
 # Solicita el nombre del usuario
 nombre = input("¿Cuál es tu nombre? ")
 
-# Solicita la edad del usuario y convierte la entrada a un número entero 
+# Solicita la edad del usuario y convierte la entrada a un número entero
 edad = int(input("¿Cuántos años tienes? "))
 
 # Calcula el año en que el usuario cumplirá 100 años
