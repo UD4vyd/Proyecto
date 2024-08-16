c8e9357463e37520e491dc121401dbe577e0b8ae
UD4vyd <davidmartinezrincon9@gmail.com>
2024-08-16 02:28:49 +0000

    si funciona estoy muy feliz

---
+++ probando.py
@@ -0,0 +1,13 @@
+print("¡Hola, mundo como estan todos!")
+
+# Solicita el nombre del usuario
+nombre = input("¿Cuál es tu nombre? ")
+
+# Solicita la edad del usuario y convierte la entrada a un número entero
+edad = int(input("¿Cuántos años tienes? "))
+
+# Calcula el año en que el usuario cumplirá 100 años
+año_cien = 2024 + (100 - edad)
+
+# Muestra un mensaje personalizado
+print(f"Hola, {nombre}! Tendrás 100 años en el año {año_cien}.")
